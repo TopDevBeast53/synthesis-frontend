e0e79f55fee1e2f3872e0e90937de81a519968c8
Ambitiont109 <45891661+Ambitiont109@users.noreply.github.com>
2022-05-26 07:19:40 +0900

    Implemented Home Responsive (#203)

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import PageSection from 'components/PageSection'
-import { PageMeta } from 'components/Layout/Page'
-import { Heading, Text, Button, Flex, Card } from 'uikit'
 import Column from 'components/Layout/Column'
-import Chain from 'uikit/components/Svg/Icons/Chain'
-import ArrowsUp from 'uikit/components/Svg/Icons/ArrowsUp'
+import { PageMeta } from 'components/Layout/Page'
+import PageSection from 'components/PageSection'
+import React from 'react'
+import { Button, Card, Flex, Heading, Text, useMatchBreakpoints } from 'uikit'
 import DeRisk from 'uikit/components/Svg/Icons/DeRisk'
 import InstantlySwap from 'uikit/components/Svg/Icons/InstantlySwap'
 import NewPositions from 'uikit/components/Svg/Icons/NewPositions'
-import SyncAlt from 'uikit/components/Svg/Icons/SyncAlt'
 import MetricsSection from './components/MetricsSection'
 
 const Home: React.FC = () => {
   const HomeSectionContainerStyles = { margin: '0', width: '100%', maxWidth: '968px' }
+  const { isMobile } = useMatchBreakpoints()
 
   return (
     <>
@@ -37,27 +35,27 @@ const Home: React.FC = () => {
         index={2}
         hasCurvedDivider={false}
       >
-        <Column style={{ width: '70%', marginBottom: '171px' , marginLeft:"auto", marginRight:"auto"}}>
-          <Heading textAlign="center" style={{ fontSize: '68px' }}>
+        <Column style={{ width: isMobile? '100%' : '70%', marginBottom: '171px' , marginLeft:"auto", marginRight:"auto"}}>
+          <Heading as="h1" scale="xxl" color="secondary" mb="24px" textAlign="center">
             Cheaper Swaps for Crypto, LP Tokens & Even Yield
           </Heading>
           <Flex mt="74px">
             
               <Button mr="12px" width="256px">
                 <a href="/swap">
-                  <Text style={{ color: '#101411', padding: '13px', fontSize: '18px', fontWeight: 500 }}>Trade Crypto</Text>
+                  <Text style={{ color: '#101411', fontWeight: 500 }}>Trade Crypto</Text>
                 </a>
               </Button>
               <Button mr="12px" width="300px" style={{ background: '#101411', border: '2px solid #ABBEFF' }}>
                 <a href="/lps-swap">
-                  <Text style={{ color: '#ABBEFF', padding: '13px', fontSize: '18px', fontWeight: 500 }}>
+                  <Text style={{ color: '#ABBEFF', fontWeight: 500 }}>
                     Trade LP Tokens
                   </Text>
                 </a>
               </Button>
               <Button width="256px" style={{ background: '#ABBEFF' }}>
                 <a href="/yield-swap" >
-                  <Text style={{ color: '#101411', padding: '13px', fontSize: '18px', fontWeight: 500 }}>Trade Yield</Text>
+                  <Text style={{ color: '#101411', fontWeight: 500 }}>Trade Yield</Text>
                 </a>
               </Button>
             
@@ -72,13 +70,13 @@ const Home: React.FC = () => {
         hasCurvedDivider={false}
       >
         <Card>
-          <Column style={{ padding: '70px', alignItems: 'center' }}>
-            <Heading textAlign="center" width="60%" style={{ fontSize: '50px', paddingBottom: '50px' }}>
+          <Column style={{ padding: isMobile ? '10px' : '70px', alignItems: 'center' }}>
+            <Heading as="h1" scale="xxl" color="secondary" textAlign="center"  width="60%" style={{ paddingBottom: '50px' }}>
               Helix provides a wide range of new opportunities
             </Heading>
             <Card style={{ background: 'rgba(16, 20, 17, 0.6)', marginBottom: '10px' }}>
-              <Flex style={{ padding: '44px 10%', width: '100%', justifyContent: 'space-between' }}>
-                <InstantlySwap style={{ width: "100px" }}/>
+              <Flex style={{ padding: isMobile ? '15px 5%':'44px 10%', width: '100%', justifyContent: 'space-between' }}>
+                <InstantlySwap style={{ width: "100px", marginRight:"5px" }}/>
                 <Column style={{ maxWidth: '70%' }}>
                   <Heading pb="13px">Instantly Swap Yield Positions at Low Cost</Heading>
                   <Text>
@@ -89,8 +87,8 @@ const Home: React.FC = () => {
               </Flex>
             </Card>
             <Card style={{ background: 'rgba(16, 20, 17, 0.6)', marginBottom: '10px' }}>
-              <Flex style={{ padding: '44px 10%', width: '100%', justifyContent: 'space-between' }}>
-                <DeRisk style={{ width: "100px" }}/>
+              <Flex style={{ padding: isMobile ? '15px 5%':'44px 10%', width: '100%', justifyContent: 'space-between' }}>
+                <DeRisk style={{ width: "100px", marginRight:"5px" }}/>
                 <Column style={{ maxWidth: '70%' }}>
                   <Heading pb="13px">Easily De-Risk By Locking in Fixed Returns</Heading>
                   <Text>
@@ -101,8 +99,8 @@ const Home: React.FC = () => {
               </Flex>
             </Card>
             <Card style={{ background: 'rgba(16, 20, 17, 0.6)', marginBottom: '10px' }}>
-              <Flex style={{ padding: '44px 10%', width: '100%', justifyContent: 'space-between' }}>
-                <NewPositions style={{ width: "100px" }}/>
+              <Flex style={{ padding: isMobile ? '15px 5%':'44px 10%', width: '100%', justifyContent: 'space-between' }}>
+                <NewPositions style={{ width: "100px", marginRight:"5px" }}/>
                 <Column style={{ maxWidth: '70%' }}>
                   <Heading pb="13px">Enter Yield Positions at Discounted Rates</Heading>
                   <Text>
