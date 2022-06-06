335215748a635f94e3f9b6a18300eafbee7f6a48
Kevin Qiang <butterflybutterpot@gmail.com>
2022-06-07 01:40:09 +0800

    adjust decimals in swap (#244)

--- src/views/Swap/components/TradePrice.tsx
+++ src/views/Swap/components/TradePrice.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Price } from 'sdk'
-import { Text, AutoRenewIcon } from 'uikit'
+import { Text, AutoRenewIcon, useTooltip } from 'uikit'
 import { StyledBalanceMaxMini } from './styleds'
 
 interface TradePriceProps {
@@ -10,7 +10,8 @@ interface TradePriceProps {
 }
 
 export default function TradePrice({ price, showInverted, setShowInverted }: TradePriceProps) {
-  const formattedPrice = showInverted ? price?.toSignificant(6) : price?.invert()?.toSignificant(6)
+  const formattedPrice = showInverted ? Number(price?.toSignificant(6)) < 0.000001 ? "<0.000001" : parseFloat(price?.toFixed(6)) : Number(price?.invert()?.toSignificant(6)) < 0.000001 ? "<0.000001" : parseFloat(price?.invert()?.toFixed(6))
+  const { targetRef, tooltip, tooltipVisible } = useTooltip('Flip Rate', { placement: 'right-end', trigger: 'hover' })
 
   const show = Boolean(price?.baseCurrency && price?.quoteCurrency)
   const label = showInverted
@@ -22,7 +23,8 @@ export default function TradePrice({ price, showInverted, setShowInverted }: Tra
       {show ? (
         <>
           {formattedPrice ?? '-'} {label}
-          <StyledBalanceMaxMini onClick={() => setShowInverted(!showInverted)}>
+          {tooltipVisible && tooltip}
+          <StyledBalanceMaxMini onClick={() => setShowInverted(!showInverted)} ref={targetRef} >
             <AutoRenewIcon width="14px" />
           </StyledBalanceMaxMini>
         </>
