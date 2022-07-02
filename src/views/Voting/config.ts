3b6fc3cfa29169c849eb720fe3dad3944f4ef869
Kevin Qiang <butterflybutterpot@gmail.com>
2022-07-02 12:32:34 +0900

    config voting (#304)

--- src/views/Voting/config.ts
+++ src/views/Voting/config.ts
@@ -1,9 +1,9 @@
 export const PROPOSALS_TO_SHOW = 10
 export const ADMINS = [
-    '0x59201fb8cb2D61118B280c8542127331DD141654', // HELIX TEAM
+    '0x50aA3d33800A1BF4B8ED76740Fd52dfB4Bb503E7', // HELIX TEAM
 ].map((address) => address.toLowerCase())
 export const IPFS_GATEWAY = 'https://gateway.ipfs.io/ipfs'
 export const SNAPSHOT_VERSION = '0.1.3'
 export const HELIX_SPACE = 'helixgeometry.eth'
-export const VOTE_THRESHOLD = 100
+export const VOTE_THRESHOLD = 500
 export const CHOICES_PRESET = ['In Favor', 'Against']
