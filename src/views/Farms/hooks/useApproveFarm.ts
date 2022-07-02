dd715992fa931d6387cdb04ff36b6e2f8c12dd45
Kevin Qiang <butterflybutterpot@gmail.com>
2022-07-02 13:00:28 +0900

    fix gaslimit overrides (#306)

--- src/views/Farms/hooks/useApproveFarm.ts
+++ src/views/Farms/hooks/useApproveFarm.ts
@@ -1,19 +1,25 @@
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 import { ethers, Contract } from 'ethers'
 import { useMasterchef } from 'hooks/useContract'
 import { useCallWithGasPrice } from 'hooks/useCallWithGasPrice'
 
 const useApproveFarm = (lpContract: Contract) => {
+    const overrides = useMemo(
+        () => ({
+            gasLimit: 9999999,
+        }),
+        [],
+    )
     const masterChefContract = useMasterchef()
     const { callWithGasPrice } = useCallWithGasPrice()
     const handleApprove = useCallback(async () => {
         const tx = await callWithGasPrice(lpContract, 'approve', [
             masterChefContract.address,
             ethers.constants.MaxUint256,
-        ])
+        ], overrides)
         const receipt = await tx.wait()
         return receipt.status
-    }, [lpContract, masterChefContract, callWithGasPrice])
+    }, [lpContract, masterChefContract, callWithGasPrice, overrides])
 
     return { onApprove: handleApprove }
 }
