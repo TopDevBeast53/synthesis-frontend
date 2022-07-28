85221ead78aa703f4641f11835a1b4001ef33e9a
Kevin <silverstar66703@gmail.com>
2022-07-29 06:34:48 +0900

    fix snapshot space

--- src/views/Voting/config.ts
+++ src/views/Voting/config.ts
@@ -1,3 +1,5 @@
+import { ChainId } from 'sdk'
+
 export const PROPOSALS_TO_SHOW = 10
 export const ADMINS = [
     '0x06aC5E1A19D227Dc92bec75F9Aae05b4e0C499A9',
@@ -5,6 +7,7 @@ export const ADMINS = [
 ].map((address) => address.toLowerCase())
 export const IPFS_GATEWAY = 'https://gateway.ipfs.io/ipfs'
 export const SNAPSHOT_VERSION = '0.1.3'
-export const HELIX_SPACE = 'silverstardev.eth'
+const chainId = process.env.REACT_APP_CHAIN_ID
+export const HELIX_SPACE = Number(chainId) === ChainId.MAINNET ? "helixgeometry.eth" : "silverstardev.eth"
 export const VOTE_THRESHOLD = 500
 export const CHOICES_PRESET = ['In Favor', 'Against']
