792d94daf453cd5124a9567f3b58e1bbc0f31508
Kevin <silverstar66703@gmail.com>
2022-08-09 01:56:42 +0900

    fix lp rewards fee in swap and farm (#386)

--- src/utils/prices.ts
+++ src/utils/prices.ts
@@ -9,7 +9,7 @@ import {
 import { Field } from '../state/swap/actions'
 import { basisPointsToPercent } from './index'
 
-const BASE_FEE = new Percent(JSBI.BigInt(25), JSBI.BigInt(10000))
+const BASE_FEE = new Percent(JSBI.BigInt(1), JSBI.BigInt(10000))
 const ONE_HUNDRED_PERCENT = new Percent(JSBI.BigInt(10000), JSBI.BigInt(10000))
 const INPUT_FRACTION_AFTER_FEE = ONE_HUNDRED_PERCENT.subtract(BASE_FEE)
 
