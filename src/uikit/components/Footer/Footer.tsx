2e96a518db96f3a8f518ddb88986bb87360cab6f
Eric Cheng <87131588+ericcheng47@users.noreply.github.com>
2022-08-19 03:07:07 +0800

    add powered by alchemy badge (#405) (#408)
    * add powered by alchemy badge
    
    * make badge smaller
    
    * center badge in mobile
    
    * remove fixed footer width
    
    * cursor: pointer on alchemy badge
    
    Co-authored-by: Eric Cheng <starlight201212@gmail.com>
    
    Co-authored-by: Ryan Carter <83258103+whiteapple602@users.noreply.github.com>

--- src/uikit/components/Footer/Footer.tsx
+++ src/uikit/components/Footer/Footer.tsx
@@ -6,24 +6,26 @@ import { StyledFooter, StyledList, StyledListItem, StyledText } from './styles'
 import SocialLinks from './Components/SocialLinks'
 import { FooterProps } from './types'
 import { HelixByGeometryBlue } from '../Svg'
-import CakePrice from "../CakePrice/CakePrice";
-import { Colors } from "../..";
+import CakePrice from '../CakePrice/CakePrice'
+import { Colors } from '../..'
+import PoweredByAlchemy from './PoweredByAlchemy'
 
-const MenuItem: React.FC<FooterProps> = ({
-  items,
-  isDark,
-  cakePriceUsd,
-  ...props
-}) => {
+const MenuItem: React.FC<FooterProps> = ({ items, isDark, cakePriceUsd, ...props }) => {
   return (
-    <StyledFooter p={['40px 50px 40px 40px', null, '96px 40px 96px 40px']} {...props} justifyContent="center">
-      <Flex flexDirection="column" width={['100%', null, '1200px;']}>
+    <StyledFooter
+      p={['10px 50px 40px 40px', null, '36px 40px 96px 40px']}
+      {...props}
+      flexDirection="column"
+      alignItems="center"
+    >
+      <PoweredByAlchemy />
+      <Flex flexDirection="column" width={['100%', null, '100%']} pt={['30px', null, '60px']}>
         <Box display={['block', null, 'none']} mb="24px">
           <Flex justifyContent="space-between" alignItems="center">
             <Flex flex={1}>
               <HelixByGeometryBlue isDark={isDark} width="130px" />
             </Flex>
-            <Flex flex={1} justifyContent='center'>
+            <Flex flex={1} justifyContent="center">
               <CakePrice cakePriceUsd={cakePriceUsd} color={darkColors.textSubtle as keyof Colors} />
             </Flex>
           </Flex>
@@ -31,12 +33,12 @@ const MenuItem: React.FC<FooterProps> = ({
         <Flex
           order={[2, null, 1]}
           flexDirection={['row', null, 'row']}
-          flexWrap={["wrap", null, null]}
+          flexWrap={['wrap', null, null]}
           justifyContent="space-between"
           alignItems="flex-start"
           mb={[null, null, null]}
         >
-          <Box display={['none', null, 'block']} style={{ alignSelf: "center", flex: "1.5" }}>
+          <Box display={['none', null, 'block']} style={{ alignSelf: 'center', flex: '1.5' }}>
             <Flex flexDirection="column" alignItems="center">
               <HelixByGeometryBlue isDark width="140px" />
               <Box mt="20px">
@@ -64,7 +66,7 @@ const MenuItem: React.FC<FooterProps> = ({
                   )}
                 </StyledListItem>
               ))}
-              {(index + 1 === items.length) && <SocialLinks mt="15px" />}
+              {index + 1 === items.length && <SocialLinks mt="15px" />}
             </StyledList>
           ))}
         </Flex>
